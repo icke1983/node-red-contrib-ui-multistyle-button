4343e814ae64b0cfebb738a773d07759e107155d
icke1983 <google@jensrothe.de>
2023-06-03 10:08:54 +0200

    Node has been published

---
+++ README.md
@@ -0,0 +1,49 @@
+# node-red-contrib-ui-multistyle-button Readme
+node-red-contrib-ui-multistyle-button is a button for the Node-RED Dashboard that can be styled by input
+
+## Install
+
+### Directly in Node-RED via palette manager
+
+Start Node-RED
+
+Select 'Manage palette' from menu top-right
+
+Click on the Install tab
+
+Search for ui-multistyle-button in the searchbar and find node-red-contrib-ui-multistyle-button
+
+### Manual install
+
+Run command on Node-RED installation directory.
+
+    npm install node-red-contrib-ui-multistyle-button
+
+## Usage
+
+That Button can be styled by the input. Therfor you have to specify the styleoptions of any value, you want.
+For each added **value**, you can set the style-properties separately.
+![values Example](images/values.PNG)
+
+You can use **fa-icons**, **mi-icon** and all other icons that are installed.
+
+The colors can be specified by name (e.g. **red**), by HEX code (e.g. **#FF0000**) or by rgb-command (e.g. **rgb (255, 0, 0)** ).
+
+Leave blank the styleoptions if you don't want to specify them.
+
+Determine which **'msg.object'** should be considered.
+It can also be any other object than **'msg.payload'**.
+You can look at **'msg.payload.power'** for example
+![msg_object Example](images/msg_object.PNG)
+
+Determine the **value** and the **destination 'msg.object'** which will be sent at buttonclick.
+you can send a **string**, a **number**, a **bool value** or the **value of an input 'msg.objeck'**
+![send_value Example](images/send_value.PNG)
+
+Special **strings** are **'toggle on off'** or **'toggel true false'**.
+That will toggle the selected **input 'msg.object'** to the **output** at buttonclick.
+![special_strings Example](images/special_strings.PNG)
+
+## License
+
+The MIT License
---
+++ ui_multistylebutton.js
@@ -0,0 +1,236 @@
+module.exports = function(RED) {
+    var ui = undefined;
+    var findStyle = function (value, config) {
+        let style = undefined;
+        /*style = {
+            icon: "",
+            iconColor: "",
+            iconShadow: "",
+            textColor: "",
+            textShadow: "",
+            bgColor: ""
+        };*/
+        try {
+            if (config.options.length > 0) {
+                config.options.forEach( function(item) {
+                    if (item.value == value) {
+                        let fIconColor = item.iconColor;
+                        let fTextColor = item.textColor;
+                        let fBgColor = item.bgColor;
+                        let fIconShadow = "";
+                        let fTextShadow = "";
+                        if (fIconColor == "") { fIconColor = "var(--nr-dashboard-widgetTextColor)"; }
+                        if (fTextColor == "") { fTextColor = "var(--nr-dashboard-widgetTextColor)"; }
+                        if (fBgColor == "") { fBgColor = "var(--nr-dashboard-widgetColor)"; }
+                        
+                        if (item.iconShadow == true) {
+                            fIconShadow = String.raw`
+                                0px 0px 5px ${fBgColor}, 
+                                0px 0px 10px ${fIconColor},
+                                0px 0px 15px ${fIconColor},
+                                0px 0px 20px ${fIconColor},
+                                0px 0px 30px ${fIconColor},
+                                0px 0px 40px ${fIconColor},
+                                0px 0px 55px ${fIconColor},
+                                0px 0px 75px ${fIconColor}`;
+                        }
+
+                        if (item.textShadow == true) {
+                            fTextShadow = String.raw`
+                                0px 0px 5px ${fBgColor}, 
+                                0px 0px 10px ${fTextColor},
+                                0px 0px 15px ${fTextColor},
+                                0px 0px 20px ${fTextColor},
+                                0px 0px 30px ${fTextColor},
+                                0px 0px 40px ${fTextColor},
+                                0px 0px 55px ${fTextColor},
+                                0px 0px 75px ${fTextColor}`;
+                        }
+                        
+                        style = {
+                            icon: item.icon,
+                            iconColor: item.iconColor,
+                            iconShadow: fIconShadow,
+                            textColor: item.textColor,
+                            textShadow: fTextShadow,
+                            bgColor: item.bgColor
+                        };
+                        
+                    } 
+                });
+            }
+            
+        }
+        catch (e) {
+            
+        }
+        return style;
+    }
+
+    var generateHTML = function (node, config) {
+        
+        var id = node.id.replace(/[^\w]/g, "");
+        var HTML = "";
+        /**/
+        var icons = "";
+        //let defaultStyle = findStyle("default", config);
+        let defaultStyle = findStyle(node.defaultStyleValue, config);
+        icons += String.raw`<ui-icon icon="{{msg.style.icon || '${defaultStyle.icon}'}}" style="color: {{msg.style.iconColor || '${defaultStyle.iconColor}'}}; text-shadow: {{msg.style.iconShadow || '${defaultStyle.iconShadow}'}};"></ui-icon>`;
+        HTML += String.raw`<md-button class="md-raised" ng-click="buttonClicked($event)" id="btn_${id}" style="background-color:{{msg.style.bgColor || '${defaultStyle.bgColor}'}};">`;
+        HTML += icons;
+        HTML += String.raw`
+        <span style="color: {{msg.style.textColor || '${defaultStyle.textColor}'}}; text-shadow: {{msg.style.textShadow || '${defaultStyle.textShadow}'}};">${config.label}</span>
+        </md-button>`;
+        
+        HTML += String.raw`<input type="hidden" ng-init="init('` + config.class + `')">`;
+        return HTML;
+    }
+    
+    function MS_Button(config) {
+        try {
+            node = this;
+            //node.defaultStyleValue = context.globe_defaultStyleValue.getValue();
+            node.defaultStyleValue = "default";
+            if (ui === undefined) {
+                ui = RED.require("node-red-dashboard")(RED);
+            }
+
+            RED.nodes.createNode(this,config);
+            
+            node.on('input', function(msg) {
+                //set payload
+                if (config.sendValueType == "msg") {
+                    node.newMsg_payload = RED.util.getMessageProperty(msg, config.sendValue);
+                }else if (config.sendValueType == "str") {
+                    node.newMsg_payload = RED.util.getMessageProperty(msg, config.payload);
+                }
+
+                if (config.topicType == "msg") {
+                    node.newMsg_topic = RED.util.getMessageProperty(msg, config.topic);
+                }
+
+                //set Style
+
+                let style = findStyle(RED.util.getMessageProperty(msg, config.payload), config);
+                if(style != undefined) {
+                    node.style = style;
+                }
+                if (node.style != undefined) {
+                    node.defaultStyleValue = RED.util.getMessageProperty(msg, config.payload);
+                    msg.style = node.style;
+                }
+            
+                
+            });
+
+            var done = ui.addWidget({
+                node: node,
+                format: generateHTML(node, config),
+                templateScope: "local",
+                group: config.group,
+                emitOnlyNewValues: false,
+                forwardInputMessages: false,
+                storeFrontEndInputAsState: true,
+                group: config.group,
+                order: config.order,
+                width: config.width,
+                height: config.height,
+                
+                convertBack: function (value) {
+                    return value;
+                },
+
+                beforeSend: function (msg, orig) {
+                    //set topic
+                    let t;
+                    if (config.topicType == "msg") { 
+                        t = node.newMsg_topic; 
+                    } else if (config.topicType == "str") {
+                        t = config.topic;
+                    }
+                    msg.topic = t;
+
+                    //set payload
+                    let pl;
+                    if (config.sendValueType == "str") {
+                        if (config.sendValue == "toggle on off") {
+                            if (node.newMsg_payload == "on") {
+                                pl = "off"
+                            } else {
+                                pl = "on"
+                            }
+                        } else if (config.sendValue == "toggle true false") {
+                            if (node.newMsg_Payload == true) {
+                                pl.payload = false
+                            } else {
+                                pl.payload = true
+                            }
+                        } else {
+                            pl = config.sendValue;    
+                        }
+                    } else if (config.sendValueType == "num") {
+                        pl = parseFloat(config.sendValue);
+                    } else if (config.sendValueType == "bool") {
+                        pl = Boolean(config.sendValue);
+                    } else if (config.sendValueType == "msg") {
+                        pl = node.newMsg_payload ;
+                    }
+                    delete msg[config.sendValueTo.split(".")[0]];
+                    RED.util.setMessageProperty(msg, config.sendValueTo, pl, true);
+                                   
+                    if (orig){
+                        msg.event = orig.msg.event  
+                    }      
+                },
+
+                initController: function ($scope, events) {
+                    setTimeout(function () {
+                        $scope.button = document.querySelector("#btn_" + id);
+                        $scope.button.parentElement.classList.add("nr-dashboard-button");
+                        $scope.button.parentElement.classList.remove("nr-dashboard-template");
+                        if ($scope.classNames != "")
+                        {
+                            $scope.classNames.split(" ").forEach( function(item) { $scope.button.parentElement.classList.add(item); } )
+                        }
+                        
+                    }, 300);
+
+                    $scope.init = function (classNames) {
+                        $scope.classNames = classNames;
+                    }
+
+                    $scope.buttonClicked = function (event) {
+                        //$scope.send($scope.msg);
+                        $scope.send({event: $scope.getPosition(event)});                   
+                                               
+                    }
+
+                   $scope.getPosition = function (event) {
+                        let bb0 = $scope.button.parentElement.getBoundingClientRect().left;
+                        let bb3 = $scope.button.parentElement.getBoundingClientRect().top;
+                        let bb1 = bb3 + $scope.button.parentElement.offsetHeight;
+                        let bb2 = bb0 + $scope.button.parentElement.offsetWidth;
+                        let x = event.clientX;
+                        let y = event.clientY;
+                        return { clientX: x, clientY: y, bbox: [bb0, bb1, bb2, bb3] };
+                    }
+
+                    $scope.$watch("msg", function (msg) {
+                        if (!msg) { return; };
+                    });
+                },
+                
+                beforeEmit: function (msg, value) {
+                    return { msg };
+                
+            }
+
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+        node.on("close", done);
+    }
+    RED.nodes.registerType("ui_multistyle-button",MS_Button);
+}
